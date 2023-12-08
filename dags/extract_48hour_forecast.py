96540a150e41710878284ea8a0e991ae442d379e
vmazeiko <v.mazeyko@smartcat.ai>
2023-12-08 04:16:48 +0300

    loading data to postgres added

--- dags/extract_48hour_forecast.py
+++ dags/extract_48hour_forecast.py
@@ -5,6 +5,7 @@ from airflow.operators.python import PythonOperator
 
 from castom_etl_lib.openweathermap_provider import openweathermap_provider
 from castom_etl_lib.json_to_csv_transformer import process_json_to_csv
+from castom_etl_lib.postgres_consumer import postgres_consumer
 
 description = ''' 
     This DAG extracts hourly weather forecast for 48 hours taken from www.openweathermap.org
@@ -26,6 +27,8 @@ default_args = {
 
 postgres_conn_id = 'postgres_local'
 openweathermap_access_token = Variable.get("openweathermap_access_token")
+target_schema = 'public'
+target_table = 'weather_forecasts'
 
 dag = DAG(
     dag_id='extract_48hour_forecast',
@@ -40,15 +43,15 @@ dag = DAG(
 )
 
 for city_name in cities:
-    # extract_node = PythonOperator(
-    #     task_id=f"extract_task_{city_name.replace(' ','_')}",
-    #     python_callable=openweathermap_provider,
-    #     op_kwargs={
-    #         'city_name': city_name,
-    #         'appid': openweathermap_access_token,
-    #     },
-    #     dag=dag,
-    # )
+    extract_node = PythonOperator(
+        task_id=f"extract_task_{city_name.replace(' ','_')}",
+        python_callable=openweathermap_provider,
+        op_kwargs={
+            'city_name': city_name,
+            'appid': openweathermap_access_token,
+        },
+        dag=dag,
+    )
 
     transform_node = PythonOperator(
         task_id=f"transform_task_{city_name.replace(' ','_')}",
@@ -58,16 +61,17 @@ for city_name in cities:
         },
         dag=dag,
     )
-    #
-    # load_node = PythonOperator(
-    #     task_id=f"load_task_{city.replace(' ','_')}",
-    #     python_callable=openweathermap_provider,
-    #     op_kwargs={
-    #         'city': city,
-    #         'appid': openweathermap_access_token,
-    #         'file_path': bronze_path,
-    #     },
-    #     dag=dag,
-    # )
+
+    load_node = PythonOperator(
+        task_id=f"load_task_{city_name.replace(' ','_')}",
+        python_callable=postgres_consumer,
+        op_kwargs={
+            'city_name': city_name,
+            'postgres_conn_id': postgres_conn_id,
+            'target_schema': target_schema,
+            'target_table': target_table,
+        },
+        dag=dag,
+    )
 
     # extract_node >> transform_node >> load_node
